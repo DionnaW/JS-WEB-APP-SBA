d97f8a5ef97615c63ea444a037025a3db40ac9fa
Dionna <businessladythick@yahoo.com>
2024-03-20 22:54:18 -0400

    got started and having api issues

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JS-WEB-APP-SBA
+I've decided to do my wep app about cats. I am a cat mom, so why not?
+
+For some reason my api key is returning 404 a dn I'm tired, so I'll be back at it after school tomorrow
--- script/index.js
+++ script/index.js
@@ -1,1 +1,5 @@
-console.log("checking 1, 2, 3");
+//console.log("checking 1, 2, 3"); //just checking to make sure everything is up and running
+
+//fetching data from cat api
+const apikey = 'live_OwhhhHYXMP56Fmf24uUzlKb1Zl5XrO3926cS4Mr2Ngcno632lTMw1v2iiG9YcsUF';
+const apiUrl = 'https://api.cat.com/v1/search?limit=11&api_key=live_OwhhhHYXMP56Fmf24uUzlKb1Zl5XrO3926cS4Mr2Ngcno632lTMw1v2iiG9YcsUF';
