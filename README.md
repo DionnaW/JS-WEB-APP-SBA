edfa5b6c7aab0615226cdc938c97dff72306d63d
Dionna <businessladythick@yahoo.com>
2024-03-23 01:58:10 -0400

    can't get POST/PATCH OR PUT 2 wrk

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 I've decided to do my wep app about cats. I am a cat mom, so why not?
 
 For some reason my api key is returning 404 a dn I'm tired, so I'll be back at it after school tomorrow
+
+3-22
+I got it on a roll now. Pictures from my cat api the change everytime the page is refreshed
+so far for the requirements i have used fetch api, used GET request to create user interaction, now i am on to the others...
--- script/index.js
+++ script/index.js
@@ -1,1 +1,25 @@
 //console.log("checking 1, 2, 3"); //just checking to make sure everything is up and running
+
+// import { TheCatAPI } from "@thatapicompany/thecatapi";
+// const API_KEY = 'api_key=live_OwhhhHYXMP56Fmf24uUzlKb1Zl5XrO3926cS4Mr2Ngcno632lTMw1v2iiG9YcsUF';
+// 
+// async function getVotes() {
+    // const votes = await theCatAPI.votes.getVotes();
+    // return votes;
+//   }
+// 
+
+
+// fetch('https://api.thecatapi.com/v1/favourites', {
+//   method: 'POST',
+//   headers: {
+    // 'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+    // title: 'My Post',
+    // content: 'This is the content of my post'
+//   })
+// })
+//   .then(response => response.json())
+//   .then(data => console.log('Post created:', data))
+//   .catch(error => console.error('Error:', error));
